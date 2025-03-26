33b904f6af07e796b723fc44a283bd0720a54248
Mark Ryan D <mark.d@freshclinics.com>
2025-03-26 21:29:13 +0800

    update login page

--- README.md
+++ README.md
@@ -34,3 +34,14 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+
+URL: https://document-finder-seven.vercel.app
+username: johndoe@example.com
+password: password
+
+features:
+- Authentication
+- Database integration
+- View all cabinets
+- View cabinet with documents
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import { getSession } from "@/auth";
 import { LoginForm } from "@/components/auth/login-form"
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
@@ -10,10 +11,18 @@ export default async function Page() {
   }
     
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
+    <>
+
+      <div className="text-center flex flex-col justify-center items-center gap-4 mt-8">
+        <Image width={80} height={80} src="/logo.jpeg" alt="Logo"></Image>
+        <h2 className="text-xl font-medium">LGU STA. MARGARITA - ACCOUNTING OFFICE</h2>
+        <h1 className="text-2xl font-bold">DOCUMENT FINDER</h1>
       </div>
-    </div>
+      <div className="flex mt-8 w-full items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm">
+          <LoginForm />
+        </div>
+      </div>
+    </>
   )
 }
